f398871488d7c0544abb80dfcdfe520d73a57238
TheHoboArk <48610477+TheHoboArk@users.noreply.github.com>
2020-04-23 23:59:52 -0500

    Final comment to Update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,3 +3,5 @@
 git -C /var/www/ pull;
 
 npm --prefix /var/www run production;
+
+# To run this: wget -O update.sh https://raw.githubusercontent.com/TheHoboArk/Community-Website/master/scripts/update.sh && bash update.sh
