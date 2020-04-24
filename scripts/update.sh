0677242a11ecfd746d2c12ae35daa66066ed92b6
TheHoboArk <48610477+TheHoboArk@users.noreply.github.com>
2020-04-24 00:03:19 -0500

    Delete update.sh

--- scripts/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git -C /var/www/ pull;
-
-npm --prefix /var/www run production;
-
-# To run this: wget -O update.sh https://raw.githubusercontent.com/TheHoboArk/Community-Website/master/scripts/update.sh && bash update.sh
